4b238b702a9e8d49f5e17991624a7d8f6e2eac69
yuyuehill <yuyuehill@gmail.com>
2012-11-07 14:05:00 +0800

    add curl util

--- tests/test_cinder_api.py
+++ tests/test_cinder_api.py
@@ -7,12 +7,13 @@ Created on Oct 28, 2012
 import httplib
 import json
 import unittest
+import commands
 
 class  TestClinder(unittest.TestCase): 
-    def setUp(self):
-        url = "hillopen:5000"
+    def _setUp(self):
+        url = "http://hillopen:5000"
         user =  "admin"
-        password = "passwo0rd"
+        password = "admin_pass"
         params = '{"passwordCredentials":{"username":user,"password":password}}'
         headers= {"Content-Type":"application/json"}
         self.conn = httplib.HTTPConnection(url)
@@ -20,13 +21,26 @@ class  TestClinder(unittest.TestCase):
         self.conn.request("POST","/v2.0/tokens",params,headers)
         response = self.conn.getresponse()
         data = response.read()
+        print "Got reponse %s" % data
         dd = json.loads(data)
         self.conn.close()
+        
         apitoken = dd['auth']['token']['id']
         print "You token is %s" % apitoken
-        
-    def test_cinder_create_volume(self):
-        
-        print "I am here"    
-        print "test again"
     
+    def test_keystone_validation(self):
+        
+        url = "http://localhost:5000/v2.0/tokens"
+        data = '{"passwordCredentials":{"username":"admin","password":"password"}}'
+        
+        command = "curl -d \'%s\' -H \"Content-Type: application/json\" %s "  % (data, url)
+        print command        
+        print commands.getoutput(command)
+        
+            
+    def test_cinder_create_volume(self):
+        abc = "hill"
+        aa = '{"I am here" :abc}'  
+        print   aa
+        print "test again"
+        print "create volume"
