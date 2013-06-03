6732bd2c3df959462c9da35b541d8733f1dde374
hill <yuyuehill@gmail.com>
2013-06-03 16:37:12 +0800

    update testmox

--- tests/lang/test_mox.py
+++ tests/lang/test_mox.py
@@ -4,3 +4,25 @@ Created on Jun 2, 2013
 @author: hill
 '''
 
+import os
+import unittest
+import mox
+
+
+class MyObject(object):
+    def __init__(self):
+        pass
+    def get_mydatas(self):
+        return "hill"
+    
+
+class TestMyObject(unittest.TestCase):
+   
+   def setup(self):
+       self.obj_mocker=mox.Mox()
+       
+   def test_get_mydates(self):
+       myobj = self.obj_mocker.CreateMock(MyObject)
+       myobj.get_mydatas()
+       mox.Replay(myobj)
+       mox.Verify(myobj)     
