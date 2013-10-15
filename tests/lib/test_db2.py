6301c232fb139aff73e5335743b2ade33c7122fe
yuyuehill <yuyuehill@gmail.com>
2013-10-15 15:49:34 +0800

    add db2 test

--- tests/lib/test_db2.py
+++ tests/lib/test_db2.py
@@ -5,8 +5,9 @@ import unittest
 
 import os
 import ssl
+import sqlalchemy
 from sqlalchemy import *
-
+import ibm_db_sa.ibm_db_sa
 
 import sqlalchemy.util as util
 import string,sys
@@ -15,11 +16,9 @@ import string,sys
 class TestDB2(unittest.TestCase):
     
     def setup(self):
-        self.engine = create_engine('mysql',
-                             {'db':'test','user':'root','passwd':'caoyan','host':'127.0.0.1'}, echo=True)  
-
-        
-    
+       db2 = sqlalchemy.create_engine('ibm_db_sa://noa14447:passw0rd@172.17.42.80:50000/openstac')
+       
+  
     def test_query(self):
         
         print self.engine
